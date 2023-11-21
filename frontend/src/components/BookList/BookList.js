ea109f977560269c0384f4737b2fbe8d0862976c
Anna Kudriashova <amarenoru@gmail.com>
2023-11-21 10:59:15 +0200

    filter Redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,10 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs';
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
+import { selectTitleFilter } from '../../redux/slices/filterSlice';
 import './BookList.css';
 
 const BookList = () => {
     const books = useSelector((state) => state.books);
+    const titleFilter = useSelector(selectTitleFilter);
     const dispatch = useDispatch();
     const handleDeleteBook = (id) => {
         dispatch(deleteBook(id));
@@ -12,6 +14,12 @@ const BookList = () => {
     const handleToggleFavorite = (id) => {
         dispatch(toggleFavorite(id));
     };
+    const filteredBooks = books.filter((book) => {
+        const matchesTitle = book.title
+            .toLowerCase()
+            .includes(titleFilter.toLowerCase());
+        return matchesTitle;
+    });
 
     return (
         <div className="app-block book-list">
@@ -20,7 +28,7 @@ const BookList = () => {
                 <p>No books available</p>
             ) : (
                 <ul>
-                    {books.map((book, i) => (
+                    {filteredBooks.map((book, i) => (
                         <li key={book.id}>
                             <div className="book-info">
                                 {++i}. {book.title} by{' '}
---
+++ frontend/src/redux/slices/filterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    title: '',
+};
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        setTitleFilter: (state, action) => {
+            // state can be mutated thanks to Immer library, redux slice approach
+            state.title = action.payload;
+
+            // you can also return new state as usually, traditional approach
+            //return { ...state, title: action.payload };
+        },
+        resetFilters: (state) => {
+            return initialState;
+        },
+    },
+});
+
+export const { setTitleFilter, resetFilters } = filterSlice.actions;
+
+export const selectTitleFilter = (state) => state.filter.title;
+
+export default filterSlice.reducer;
